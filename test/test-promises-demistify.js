f28daedb27dfeb264f0d19ade256f3a133e963c8
Raymond Ho <rayho@paypal.com>
2016-01-17 12:17:49 +0800

    initial commit

---
+++ test/test-promises-demistify.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+
+import Test from 'tape';
+import Promises-demistify from '../dist/lib';
+
+Test('test promises-demistify', (t) => {
+
+    t.test('plan', (t) => {
+        t.plan(1);
+
+        t.ok(true);
+    });
+
+});
